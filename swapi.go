0a8d1d9c6fdc3256e86760567427e6d5ef2f68c6
Alexander Thompson <amthompson42010@gmail.com>
2025-02-11 20:10:58 -0600

    updated to show  a little more odf a go structure

--- swapi.go
+++ swapi.go
@@ -1,41 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	"log"
 )
 
+type Person struct {
+	Name      string   `json:"name"`
+	Height    string   `json:"height"`
+	Mass      string   `json:"mass"`
+	HairColor string   `json:"hair_color"`
+	SkinColor string   `json:"skin_color"`
+	EyeColor  string   `json:"eye_color"`
+	BirthYear string   `json:"birth_year"`
+	Gender    string   `json:"gender"`
+	Homeworld string   `json:"homeworld"`
+	Films     []string `json:"films"`
+	Species   []string `json:"species"`
+	Vehicles  []string `json:"vehicles"`
+	Starships []string `json:"starships"`
+	Created   string   `json:"created"`
+	Edited    string   `json:"edited"`
+	URL       string   `json:"url"`
+}
+
 func main() {
-	// Define the API endpoint URL
 	url := "https://swapi.dev/api/people/1"
 
-	// Create a new HTTP client
-	client := &http.Client{}
-
-	// Create a new GET request
-	req, err := http.NewRequest("GET", url, nil)
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-    // Optionally add headers to the request
-	req.Header.Add("Accept", "application/json")
-
-	// Send the request
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error:", err)
+		return
 	}
 	defer resp.Body.Close()
 
-	// Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error:", err)
+		return
 	}
 
-	// Print the response body
-	fmt.Println(string(body))
+	var person Person
+	err = json.Unmarshal(body, &person)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Printf("Name: %s\n", person.Name)
+	fmt.Printf("Height: %s\n", person.Height)
+	fmt.Printf("Mass: %s\n", person.Mass)
 }
